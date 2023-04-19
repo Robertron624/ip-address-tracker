6c7db78d372bb218e373c78daede957bfe48fb09
Roberto Ramirez <robert.624@hotmail.com>
2023-04-18 22:13:28 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>IP Address Tracker</h1>
+    </div>
+  )
+}
+
+export default App
