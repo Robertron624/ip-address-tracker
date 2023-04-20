c9ca839ecbc9561dcc451d4c91571ecaff4ee493
Roberto Ramirez <robert.624@hotmail.com>
2023-04-20 18:52:58 -0500

    handled form

--- src/App.tsx
+++ src/App.tsx
@@ -10,35 +10,69 @@ const API_KEY = import.meta.env.VITE_IPIFY_API_KEY;
 
 const BASE_URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`;
 
+interface RootObject {
+  ip: string;
+  location: Location;
+  as: As;
+  isp: string;
+}
+
+interface As {
+  asn: number;
+  name: string;
+  route: string;
+  domain: string;
+  type: string;
+}
+
+interface Location {
+  country: string;
+  region: string;
+  city: string;
+  lat: number;
+  lng: number;
+  postalCode: string;
+  timezone: string;
+  geonameId: number;
+}
+
 function App() {
     const [ip, setIp] = useState("");
-    const {data, error, loading} = UseIp(`${BASE_URL}&ipAddress=${ip}`);
+    const { data, error, loading } = UseIp(`${BASE_URL}&ipAddress=${ip}`);
 
-    if(loading) return <div>Loading...</div>
+    if (loading) return <div>Loading...</div>;
 
-    if(error) return <div>There was an error: {error}</div>
+    if (error) return <div>There was an error: {error}</div>;
+
+    console.log(data)
 
     return (
         <div className="App">
             <div className="top-app">
                 <h1>IP Address Tracker</h1>
                 <div className="ip-form">
-                    <IpForm />
-                    {data ?  <IpDetails ipDetails={data} /> : null}         
+                    <IpForm setIp={setIp} />
+                    {data ? <IpDetails ipDetails={data} /> : null}
                 </div>
             </div>
             <div className="bottom-app">
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>     
-                </Marker>
-            </MapContainer>
+                {data ? (
+                    <MapContainer
+                        center={[data.location.lat, data.location.lng]}
+                        zoom={13}
+                        scrollWheelZoom={false}
+                    >
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[51.505, -0.09]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                ) : null}
             </div>
         </div>
     );
--- src/components/IpForm.tsx
+++ src/components/IpForm.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './index.scss'
 
-const IpForm = () => {
+const IpForm = ({setIp}: any) => {
+
+  function handleSubmit (e: React.SyntheticEvent) {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      ipNumber: { value: string };
+    };
+    const ip = target.ipNumber.value;
+    console.log("ip to search -> ", ip)
+    setIp(ip);
+  }
 
   return (
-    <form action="">
-        <label htmlFor="ip-number">Ip number</label>
-        <input type="text" />
+    <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="ipNumber">Ip number</label>
+        <input type="text" name='ipNumber'/>
         <button type='submit'>
             <img src="icon-arrow.svg" alt="arrow" />
         </button>
